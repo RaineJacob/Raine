5fd575f29a5f5350ecdaf6a984ba6ca6dad1779f
RaineJacob <119743501+RaineJacob@users.noreply.github.com>
2025-05-25 21:09:08 +0530

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import NewsletterImage from '../images/NewsletterImage.png';
-import RogfreeImage from '../images/RogfreeImage.png';
-import TindogImage from '../images/TindogImage.png';
-import WigglesImage from '../images/WigglesImage.png';
+import HotelBookingImage from '../images/HotelBookingImage.png';
+import DeliveryDelayImage from '../images/DeliveryDelayImage.png';
+import MarketingAnalysisImage from '../images/MarketingAnalysisImage.png';
+import HrAnalyticsImage from '../images/HrAnalyticsImage.png';
 
 const Projects = () => {
   return (
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={WigglesImage} projectName="Wiggles" />
-        <ProjectBox projectPhoto={NewsletterImage} projectName="Newsletter" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="RogFree" />
-        <ProjectBox projectPhoto={TindogImage} projectName="Tindog" />
+        <ProjectBox projectPhoto={HotelBookingImage} projectName="Hotel Booking Analysis" />
+        <ProjectBox projectPhoto={DeliveryDelayImage} projectName="Delivery Delay Analysis" />
+        <ProjectBox projectPhoto={MarketingAnalysisImage} projectName="Marketing Analysis" />
+        <ProjectBox projectPhoto={HrAnalyticsImage} projectName="Hr Analytics" />
       </div>
 
     </div>
