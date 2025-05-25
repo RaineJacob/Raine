6dda80f0b979a1e761445e614aad32b379dfdba5
RaineJacob <119743501+RaineJacob@users.noreply.github.com>
2025-05-25 22:10:16 +0530

    Update Skills.js

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,39 +1,38 @@
 import React from 'react'; 
+import { CgCPlusPlus } from "react-icons/cg";
 import { FaPython, FaGitAlt, FaGithub, FaFigma } from "react-icons/fa";
 import { 
-    SiVercel, 
-    SiMysql, 
-    SiPowerbi, 
-    SiJupyter, 
-    SiPostgresql, 
-    SiTableau, 
-    SiMicrosoftoffice, 
-    SiAmazonaws, 
-    SiWindows 
+  SiVercel, 
+  SiMysql, 
+  SiPowerbi, 
+  SiJupyter, 
+  SiPostgresql, 
+  SiTableau, 
+  SiMicrosoftoffice, 
+  SiAmazonaws, 
+  SiMicrosoftazure 
 } from "react-icons/si";
 
 const Skills = ({ skill }) => {
-        const icon = {
-      Python: <FaPython />,
-      Jupyter: <SiJupyter />,
-      MySQL: <SiMysql />,
-      Postgres: <SiPostgresql />,
-      Git: <FaGitAlt />,
-      Github: <FaGithub />,
-      PowerBI: <SiPowerbi />,
-      Excel: <SiMicrosoftoffice />,
-      'MS Office': <SiMicrosoftoffice />,
-      Figma: <FaFigma />,
-      Vercel: <SiVercel />,
-      Tableau: <SiTableau />,
-      'AWS (Basics)': <SiAmazonaws />,
-};
-
+    const icon = {
+        Git: <FaGitAlt />, 
+        Github: <FaGithub />, 
+        Figma: <FaFigma />, 
+        Vercel: <SiVercel />, 
+        Python: <FaPython />, 
+        Jupyter: <SiJupyter />, 
+        MySQL: <SiMysql />, 
+        Postgres: <SiPostgresql />, 
+        PowerBI: <SiPowerbi />, 
+        Excel: <SiMicrosoftoffice />, 
+        'MS Office': <SiMicrosoftoffice />, 
+        Tableau: <SiTableau />, 
+        AWS: <SiAmazonaws />
     };
 
     return (
         <div title={skill} className='SkillBox'>
-            {icon[skill] || <SiWindows />} {/* fallback icon */}
+            {icon[skill] || <span>❓</span>} {/* fallback icon if missing */}
         </div>
     );
 };
